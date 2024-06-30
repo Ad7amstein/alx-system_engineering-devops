8e2d156a262fc9b645ddab812a405988c0ac1957
Ad7amstein <adham32003200@gmail.com>
2024-06-30 09:22:30 -0700

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Create a file
+
+file { '/tmp/school':
+  ensure  => present,
+  path    => '/tmp/school',
+  group   => 'www-data',
+  owner   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
