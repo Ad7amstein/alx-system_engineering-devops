e94e1aeeb8c7fdac888418a21dc15a28be805c69
Ad7amstein <adham32003200@gmail.com>
2024-06-30 10:18:42 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
 #!/usr/bin/pup
-# Install a package
 
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install a package
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
