5b8284c2404e174ff1e2f2101cddd0c1647e72b9
Ad7amstein <adham32003200@gmail.com>
2024-07-01 04:29:43 -0700

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,22 @@
+#Client configuratino file (w/Puppet)
+
+# Ensure the .ssh directory exists
+file { '/root/.ssh':
+  ensure => directory,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0700',
+}
+
+$str = "Host 342067-web-01
+    HostName 3.84.237.204
+    IdentityFile /root/.ssh/school
+    PasswordAuthentication no
+"
+
+file { '/root/.ssh/ssh_config':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  content => $str
+}
